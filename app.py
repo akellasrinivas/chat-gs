10a5d181dfeadca86dbb494dd19cfaf7db3d652e
Akella srinivasa chakravarthy <141471061+akellasrinivas@users.noreply.github.com>
2023-12-01 14:54:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,39 +12,6 @@ def fetch_data(query):
     # Placeholder logic, replace with your actual data retrieval logic
     return f"Data fetched for: {query}"
 
-# Main Streamlit app
-def main():
-    st.title("Voice-based Geospatial Chatbot")
-
-    # Text input for user queries
-    user_input = st.text_input("Enter your query:")
-
-    # Button to process the query
-    if st.button("Process Query"):
-        # Process user query and display result
-        processing_result = process_query(user_input)
-        st.info(processing_result)
-
-        # Simulate fetching data based on the processed query
-        data_result = fetch_data(processing_result)
-        st.success(data_result)
-
-    # Navigation bar
-    menu = ["Home", "History"]
-    choice = st.sidebar.selectbox("Navigation", menu)
-
-    # Display content based on the selected navigation choice
-    if choice == "Home":
-        st.write("Welcome to the Home section!")
-    elif choice == "History":
-        st.write("View your chat history here.")
-
-    # Theme switch button
-    if st.button("Switch Theme"):
-        current_theme = st.get_option("theme")
-        new_theme = "light" if current_theme == "dark" else "dark"
-        st.set_option("theme", new_theme)
-        st.experimental_rerun()
 
 # Run the Streamlit app
 if __name__ == "__main__":
