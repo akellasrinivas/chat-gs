44ca90ed0141aa2e6302d6000edb0dfc0a9c1e32
Akella srinivasa chakravarthy <141471061+akellasrinivas@users.noreply.github.com>
2023-12-01 14:29:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,107 +1,49 @@
 import streamlit as st
-import ee
-import geemap
 
-# Initialize the Earth Engine module.
-ee.Initialize()
+# Function to simulate chatbot processing
+def process_query(user_input):
+    # Placeholder logic, replace with your actual processing logic
+    return f"Processing: {user_input}"
 
-# Authentication
-if not ee.data._credentials:
-    st.warning("You need to authenticate Earth Engine. Click the button below to authenticate.")
-    if st.button("Authenticate Earth Engine"):
-        ee.Authenticate()
-        ee.Initialize()
-
-# Function to import a shapefile from GEE asset and add predefined layers
-def import_and_add_layers(asset_id, predefined_layers=None):
-    # Import the shapefile as an Earth Engine FeatureCollection
-    shp = ee.FeatureCollection(asset_id)
-
-    # Add predefined layers
-    if predefined_layers:
-        shp = shp.map(lambda feature: feature.set(predefined_layers))
-
-    return shp
-
-# Function to add JRC Global Surface Water Mapping Layer, v1.4, to a specific ROI
-def add_jrc_layer_to_roi(shapefile, jrc_layer, map_obj):
-    # Clip JRC layer to the current ROI
-    jrc_layer_clipped = jrc_layer.clip(shapefile)
-
-    # Add the clipped JRC layer to the map
-    map_obj.addLayer(shapefile, {}, 'Selected ROI')
-    map_obj.addLayer(jrc_layer_clipped, {'palette': 'blue'}, 'Clipped JRC Global Surface Water')
-
-# Function to add SAR (COPERNICUS) layer mean for a specific date range to a given ROI
-def add_sar_layer_to_clipped_roi(shapefile, date_range, map_obj):
-    # Load the SAR (COPERNICUS) ImageCollection
-    sar_collection = ee.ImageCollection('COPERNICUS/S1_GRD') \
-        .filterBounds(shapefile) \
-        .filterDate(ee.DateRange(date_range.split(' to ')[0], date_range.split(' to ')[1]))
-
-    # Calculate the mean for the specified date range
-    sar_mean = sar_collection.mean()
-
-    # Clip SAR mean to the current ROI
-    sar_mean_clipped = sar_mean.clip(shapefile)
-
-    # Add the clipped SAR layer mean to the map
-    map_obj.addLayer(sar_mean_clipped, {'bands': ['VV'], 'min': -20, 'max': 0, 'gamma': 1.4}, 'SAR (COPERNICUS) Mean')
+# Function to simulate fetching data based on the user's query
+def fetch_data(query):
+    # Placeholder logic, replace with your actual data retrieval logic
+    return f"Data fetched for: {query}"
 
 # Main Streamlit app
 def main():
-    st.title("GEE Chatbot Interface")
+    st.title("Voice-based Geospatial Chatbot")
 
-    # List of asset IDs for the shapefiles in your GEE account
-    asset_ids = [
-        'projects/ee-my-srinivas/assets/himayatsagar',
-        'projects/ee-my-srinivas/assets/hussansagar',
-        'projects/ee-my-srinivas/assets/osmansagar',
-        'projects/ee-my-srinivas/assets/sriramsagar',
-        'projects/ee-my-srinivas/assets/karanja'
-    ]
+    # Text input for user queries
+    user_input = st.text_input("Enter your query:")
 
-    # Import and add layers for each shapefile
-    shapefiles = [import_and_add_layers(asset_id) for asset_id in asset_ids]
+    # Button to process the query
+    if st.button("Process Query"):
+        # Process user query and display result
+        processing_result = process_query(user_input)
+        st.info(processing_result)
 
-    # Add JRC Global Surface Water Mapping Layer, v1.4, only to the imported shapefiles
-    jrc_layer = ee.Image('JRC/GSW1_4/GlobalSurfaceWater').select('occurrence')
+        # Simulate fetching data based on the processed query
+        data_result = fetch_data(processing_result)
+        st.success(data_result)
 
-    # User input for selecting a specific ROI and date range
-    user_query = st.text_input("Enter your query:")
+    # Navigation bar
+    menu = ["Home", "History"]
+    choice = st.sidebar.selectbox("Navigation", menu)
 
-    # Extract ROI name and date range from the user's query
-    try:
-        roi_name = user_query.split(" of ")[-1].split(" from ")[0].strip()
-        date_range = user_query.split(" from ")[-1].strip()
-    except IndexError:
-        st.warning("Invalid query format. Please enter a valid query.")
-        roi_name = None
-        date_range = None
+    # Display content based on the selected navigation choice
+    if choice == "Home":
+        st.write("Welcome to the Home section!")
+    elif choice == "History":
+        st.write("View your chat history here.")
 
-    # Check if ROI name and date range are extracted successfully
-    if roi_name and date_range:
-        # Check if the entered ROI name is valid
-        valid_roi_names = ['himayatsagar', 'hussansagar', 'osmansagar', 'sriramsagar', 'karanja']
-        if roi_name.lower() in valid_roi_names:
-            # Filter shapefiles based on the user's specified ROI
-            selected_roi = shapefiles[valid_roi_names.index(roi_name.lower())]
+    # Theme switch button
+    if st.button("Switch Theme"):
+        current_theme = st.get_option("theme")
+        new_theme = "light" if current_theme == "dark" else "dark"
+        st.set_option("theme", new_theme)
+        st.experimental_rerun()
 
-            # Visualize the shapefiles and add the clipped JRC layer to each ROI
-            Map = geemap.Map()
-            add_jrc_layer_to_roi(selected_roi, jrc_layer, Map)
-            add_sar_layer_to_clipped_roi(selected_roi, date_range, Map)
-            Map.centerObject(selected_roi, 10)  # Center the map on the selected ROI
-            Map.addLayerControl()
-
-            # Display the map using Streamlit's map function
-            st.pydeck_chart(Map)
-
-        else:
-            st.warning("Invalid ROI name. Please enter a valid ROI name.")
-
-    else:
-        st.warning("Invalid query format. Please enter a valid query.")
-
+# Run the Streamlit app
 if __name__ == "__main__":
     main()
