d52a7d5b4f73432b19378ab9e279f4af413195c4
Akella srinivasa chakravarthy <141471061+akellasrinivas@users.noreply.github.com>
2023-12-01 15:00:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+# app.py
+
 import streamlit as st
+import speech_recognition as sr
 
 # Function to simulate chatbot processing
 def process_query(user_input):
@@ -10,17 +13,52 @@ def fetch_data(query):
     # Placeholder logic, replace with your actual data retrieval logic
     return f"Data fetched for: {query}"
 
+# Function to convert voice to text
+def convert_voice_to_text():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        st.info("Speak now...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source, timeout=5)
+
+    try:
+        st.info("Processing voice input...")
+        user_input = recognizer.recognize_google(audio)
+        return user_input
+    except sr.UnknownValueError:
+        st.warning("Sorry, could not understand the audio.")
+        return None
+    except sr.RequestError as e:
+        st.error(f"Could not request results from Google Speech Recognition service; {e}")
+        return None
+
 # Main Streamlit app
 def main():
     st.title("Voice-based Geospatial Chatbot")
 
-    # Text input for user queries
-    user_input = st.text_input("Enter your query:")
+    # Button to trigger voice input
+    if st.button("Start Voice Input"):
+        # Convert voice to text
+        user_input = convert_voice_to_text()
 
-    # Button to process the query
-    if st.button("Process Query"):
+        if user_input:
+            # Process user query and display result
+            processing_result = process_query(user_input)
+            st.info(processing_result)
+
+            # Simulate fetching data based on the processed query
+            data_result = fetch_data(processing_result)
+            st.success(data_result)
+
+    # Text input for user queries
+    st.subheader("Or, enter your query manually:")
+    manual_input = st.text_input("Enter your query:")
+
+    # Button to process the manual input
+    if st.button("Process Manual Query"):
         # Process user query and display result
-        processing_result = process_query(user_input)
+        processing_result = process_query(manual_input)
         st.info(processing_result)
 
         # Simulate fetching data based on the processed query
@@ -41,9 +79,4 @@ def main():
     if st.button("Switch Theme"):
         current_theme = st.get_option("theme")
         new_theme = "light" if current_theme == "dark" else "dark"
-        st.set_option("theme", new_theme)
-        st.experimental_rerun()
-
-# Run the Streamlit app
-if __name__ == "__main__":
-    main()
+       
