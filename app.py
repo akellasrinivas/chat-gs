7ba73bd06f5f816a7b8e87ec81f9ab930409eb3f
Akella srinivasa chakravarthy <141471061+akellasrinivas@users.noreply.github.com>
2023-12-04 14:38:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,43 @@
 import streamlit as st
-import numpy as np
-from io import BytesIO
+import subprocess
 from scipy.io.wavfile import write as wav_write
-from datetime import datetime
-from your_previous_script import get_audio, date_parser, LocationExtractor, MapVisualizer
+from sar_analysis import get_audio, date_parser, LocationExtractor, MapVisualizer
 
-# Function to convert base64 audio data to WAV
-def base64_to_wav(base64data):
-    binary = BytesIO(base64data)
+# Function to capture audio and return transcription
+def transcribe_audio():
     audio, sr = get_audio()
-    return audio, sr
+    wav_write("audio.wav", sr, audio)
 
-# Function to transcribe audio
-def transcribe_audio(audio):
-    wav_write("audio.wav", audio[1], audio[0])
-    # Perform audio transcription here using speech_recognition or any other library
-    # For demonstration purposes, let's assume the text is transcribed
-    return "This is a sample transcription."
+    # Use subprocess to run the existing code with the transcribed text
+    result = subprocess.run(["python", "sar_analysis.py"], capture_output=True, text=True)
+    return result.stdout
 
-# Function to perform SAR analysis
-def perform_sar_analysis(user_text):
-    start_date, end_date = date_parser(user_text)
-    location_extractor = LocationExtractor(csv_file_path='/content/SF.csv')
-    selected_roi_name = location_extractor.extract_entities(user_text)
-    map_visualizer = MapVisualizer()
-    _, conclusion = map_visualizer.run_analysis(
-        asset_ids=[],  # Provide your asset IDs
-        selected_roi_name=selected_roi_name,
-        start_date=start_date,
-        end_date=end_date,
-        csv_file_path='/content/SF.csv'
-    )
-
-    return conclusion
-
-# Streamlit App
 def main():
     st.title("SAR Analysis with Voice Input")
 
-    # Voice Input
-    st.subheader("Voice Input:")
-    audio_data = st.text_area("Click the record button and speak...", height=100)
-    if st.button("Record"):
-        # Assuming audio_data is in base64 format
-        audio, _ = base64_to_wav(audio_data)
-        transcription = transcribe_audio(audio)
-        st.text("Transcription:")
-        st.write(transcription)
+    # Button to trigger voice input and analysis
+    if st.button("Start Voice Input"):
+        st.info("Speak now...")
+        transcription = transcribe_audio()
+        st.success("Voice input transcribed successfully.")
 
-        # Perform SAR Analysis
-        sar_input = st.text_area("Enter text command for SAR analysis:")
-        if st.button("Run SAR Analysis"):
-            sar_result = perform_sar_analysis(sar_input)
+        # Display transcribed text
+        st.subheader("Transcribed Text:")
+        st.text(transcription)
 
-            # Display SAR Analysis results
-            st.subheader("SAR Analysis Results:")
-            st.text(sar_result)
+        # Extract information using existing code
+        start_date, end_date = date_parser(transcription)
+        location_extractor = LocationExtractor("/content/SF.csv")
+        selected_roi_name = location_extractor.extract_entities(transcription)
+        map_visualizer = MapVisualizer()
+        static_map, conclusion = map_visualizer.run_analysis(asset_ids, selected_roi_name, start_date, end_date, "/content/SF.csv")
+
+        # Display map and analysis results
+        st.subheader("Map Visualization:")
+        st.pyplot(static_map.to_pyplot())
+
+        st.subheader("Analysis Results:")
+        st.text(conclusion)
 
 if __name__ == "__main__":
     main()
