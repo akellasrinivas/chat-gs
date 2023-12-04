12b5ce83c73a50624703fa5c2504db26bbe01949
Akella srinivasa chakravarthy <141471061+akellasrinivas@users.noreply.github.com>
2023-12-04 14:10:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,61 @@
 import streamlit as st
+import numpy as np
+from io import BytesIO
+from scipy.io.wavfile import write as wav_write
+from datetime import datetime
+from your_previous_script import get_audio, date_parser, LocationExtractor, MapVisualizer
 
-# Function to simulate chatbot processing
-def process_query(user_input):
-    # Placeholder logic, replace with your actual processing logic
-    return f"Processing: {user_input}"
+# Function to convert base64 audio data to WAV
+def base64_to_wav(base64data):
+    binary = BytesIO(base64data)
+    audio, sr = get_audio()
+    return audio, sr
 
-# Function to simulate fetching data based on the user's query
-def fetch_data(query):
-    # Placeholder logic, replace with your actual data retrieval logic
-    return f"Data fetched for: {query}"
+# Function to transcribe audio
+def transcribe_audio(audio):
+    wav_write("audio.wav", audio[1], audio[0])
+    # Perform audio transcription here using speech_recognition or any other library
+    # For demonstration purposes, let's assume the text is transcribed
+    return "This is a sample transcription."
 
-# Main Streamlit app
+# Function to perform SAR analysis
+def perform_sar_analysis(user_text):
+    start_date, end_date = date_parser(user_text)
+    location_extractor = LocationExtractor(csv_file_path='/content/SF.csv')
+    selected_roi_name = location_extractor.extract_entities(user_text)
+    map_visualizer = MapVisualizer()
+    _, conclusion = map_visualizer.run_analysis(
+        asset_ids=[],  # Provide your asset IDs
+        selected_roi_name=selected_roi_name,
+        start_date=start_date,
+        end_date=end_date,
+        csv_file_path='/content/SF.csv'
+    )
+
+    return conclusion
+
+# Streamlit App
 def main():
-    st.title("Voice-based Geospatial Chatbot")
+    st.title("SAR Analysis with Voice Input")
 
-    # Text input for user queries
-    user_input = st.text_input("Enter your query:")
+    # Voice Input
+    st.subheader("Voice Input:")
+    audio_data = st.text_area("Click the record button and speak...", height=100)
+    if st.button("Record"):
+        # Assuming audio_data is in base64 format
+        audio, _ = base64_to_wav(audio_data)
+        transcription = transcribe_audio(audio)
+        st.text("Transcription:")
+        st.write(transcription)
 
-    # Button to process the query
-    if st.button("Process Query"):
-        # Process user query and display result
-        processing_result = process_query(user_input)
-        st.info(processing_result)
+        # Perform SAR Analysis
+        sar_input = st.text_area("Enter text command for SAR analysis:")
+        if st.button("Run SAR Analysis"):
+            sar_result = perform_sar_analysis(sar_input)
 
-        # Simulate fetching data based on the processed query
-        data_result = fetch_data(processing_result)
-        st.success(data_result)
+            # Display SAR Analysis results
+            st.subheader("SAR Analysis Results:")
+            st.text(sar_result)
 
-    # Navigation bar
-    menu = ["Home", "History"]
-    choice = st.sidebar.selectbox("Navigation", menu)
-
-    # Display content based on the selected navigation choice
-    if choice == "Home":
-        st.write("Welcome to the Home section!")
-    elif choice == "History":
-        st.write("View your chat history here.")
-
-    # Theme switch button
-    if st.button("Switch Theme"):
-        current_theme = st.get_option("theme")
-        new_theme = "light" if current_theme == "dark" else "dark"
-        st.set_option("theme", new_theme)
-        st.experimental_rerun()
-
-# Run the Streamlit app
 if __name__ == "__main__":
     main()
