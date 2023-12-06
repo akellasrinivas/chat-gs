7b03da68f27f92ece82786d88a7424ccf1c03f80
Akella srinivasa chakravarthy <141471061+akellasrinivas@users.noreply.github.com>
2023-12-06 15:14:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ class SARAnalyzer:
             static_map.centerObject(self.selected_roi, 10)
 
             # Display the map using Streamlit
-            st.session_state.Map = folium.Map() 
+            st.session_state.Map = folium.Map(static_map) 
             rendered_map = st_folium(st.session_state.Map)
             # Set the start_date and end_date attributes
             self.start_date = datetime.strptime(start_date, "%Y-%m-%d")
