134e76ececa2bb8518e2f8cd51bc78d4badfbba4
Akella srinivasa chakravarthy <141471061+akellasrinivas@users.noreply.github.com>
2023-12-18 10:39:09 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,4 @@
+import spacy
+model_url = 'https://github.com/explosion/spacy-models/releases/download/en_core_web_trf-3.7.3/en_core_web_trf-3.7.3-py3-none-any.whl'
+
+nlp = spacy.load('https://github.com/explosion/spacy-models/releases/download/en_core_web_trf-3.7.3/en_core_web_trf-3.7.3-py3-none-any.whl')
